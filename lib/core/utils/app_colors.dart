7d36dba6a0f4a988b25fdaf69ff08d51ba59b61e
Mohamed Ehab <mody36392@gmail.com>
2026-08-17 14:29:49 +0300

    + added Intro Screen first onboarding screen . + added easy localization package . + added extension of mediaquery .

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -5,7 +5,8 @@ class AppColors {
   static const Color redColor = Color(0xFFE82626);
   static const Color greenColor = Color(0xFF57AA53);
   static const Color whiteColor = Color(0xFFFFFFFF);
-  static const Color primaryColor = Color(0xFFFFBB3B);
+  static const Color lightwhiteColor = Color(0x99FFFFFF);
+  static const Color primaryColor = Color(0xFFF6BD00);
   static const Color darkGrey = Color(0xFF282A28);
   static const Color blackColor = Color(0xFF121312);
 }
---
+++ lib/screens/onboarding/on_boarding2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding2 extends StatelessWidget {
+  const OnBoarding2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

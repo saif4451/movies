10ff470de0a94a4bd20ff04b1233e7502d374370
Mohamed Ehab <mody36392@gmail.com>
2026-08-28 19:04:10 +0300

    +  Merge register to dev

--- lib/core/utils/Model/movie_suggestion/Movie_Suggstion.dart
+++ lib/core/utils/Model/movie_suggestion/Movie_Suggstion.dart
@@ -17,12 +17,12 @@ class MovieSuggstion {
     status = json['status'];
     statusMessage = json['status_message'];
     data = json['data'] != null ? Data.fromJson(json['data']) : null;
-    meta = json['@meta'] != null ? @meta.fromJson(json['@meta']) : null;
+    meta = json['@meta'] != null ? Meta.fromJson(json['@meta']) : null;
   }
   String? status;
   String? statusMessage;
   Data? data;
-  @meta? meta;
+  Meta? meta;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
